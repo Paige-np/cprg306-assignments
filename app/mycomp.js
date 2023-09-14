cc4e32b6251977ef8367ed65d3811511b1a83166
Paige-np <parinthorn.ph@gmail.com>
2023-09-14 14:42:26 -0600

    .

---
+++ app/mycomp.js
@@ -0,0 +1,13 @@
+
+export default function MyComp() {
+  return (
+    <main>
+      <h2>My Component</h2>
+      <p>Hello</p>
+      
+      
+    </main>
+    
+    
+  )
+}
---
+++ app/week2/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import StudentInfo from "../studentinfo";
+
+
+export default function Page() {
+  return (
+    <main>
+      <h1>My Shopping List</h1>
+      <StudentInfo />
+      
+    </main>
+  );
+}
