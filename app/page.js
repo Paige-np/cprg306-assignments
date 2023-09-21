1ec3e4d3eb54203b1621c95ce602c4981f87c588
Paige-np <parinthorn.ph@gmail.com>
2023-09-21 14:15:37 -0600

    A3 update

--- app/page.js
+++ app/page.js
@@ -4,9 +4,9 @@ import StudentInfo from './studentinfo'
 
 export default function Home() {
   return (
-    <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <StudentInfo/>
+    <main className="min-h-screen p-10">
+      <h1 className="text-xl font-bold">CPRG 306: Web Development 2 - Assignments</h1>
+      <StudentInfo/><br/>
       <p><Link href="/week2">week 2</Link></p>
       <p><Link href="/week3">week 3</Link></p>
       
--- app/studentinfo.js
+++ app/studentinfo.js
@@ -4,11 +4,11 @@ import Link from "next/link";
 export default function StudentInfo() {
   return (
     <main>
-      
-      <p>Name: Parinthorn Songsana</p>
-      <p>Course section: CPRG 306 F</p>
-      <Link href="https://github.com/Paige-np/cprg306-assignments">https://github.com/Paige-np/cprg306-assignments</Link>
-      
+      <div className="">      
+        <p>Name: Parinthorn Songsana</p>
+        <p>Course section: CPRG 306 F</p>
+        <Link href="https://github.com/Paige-np/cprg306-assignments">https://github.com/Paige-np/cprg306-assignments</Link>
+      </div>
 
   
     </main>
